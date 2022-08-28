3273f32939110b5692a176816c6501cd9809f587
wled-install <96419931+wled-install@users.noreply.github.com>
2022-08-28 16:30:46 +0200

    Update build.py

--- scripts/build.py
+++ scripts/build.py
@@ -8,7 +8,7 @@ if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 if os.path.exists(output_bin_dir):
     shutil.rmtree(output_bin_dir)
-shutil.copytree(bin_dir,output_dir)
+shutil.copytree(bin_dir,output_bin_dir)
 f= open(os.path.join(output_dir,"index_new.html"),"w+")
 for i in range(3):
      f.write("This is line %d\r\n" % (i+1))
