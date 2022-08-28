7be35ad01937ce7930cfba9de2ef95ec813b82ee
wled-install <96419931+wled-install@users.noreply.github.com>
2022-08-28 17:11:23 +0200

    Update build.py

--- scripts/build.py
+++ scripts/build.py
@@ -23,7 +23,7 @@ if not os.path.exists(output_manifest_dir):
 f_index= open(os.path.join(output_dir,"index_new.html"),"w+")
 dirs1=sorted(os.listdir(output_bin_dir))
 for dir1 in dirs1:
-    if os.path.isdir(os.path.join(output_bin_dir,dir1))
-     f_index.write(dir1+"\n")
+    if os.path.isdir(os.path.join(output_bin_dir,dir1)):
+        f_index.write(dir1+"\n")
 f_index.close()
 
