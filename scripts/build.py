95b4939a24f4f2e2a38f2c4e2b58e38141c3709f
wled-install <96419931+wled-install@users.noreply.github.com>
2022-12-12 22:37:45 +0100

    Update build.py

--- scripts/build.py
+++ scripts/build.py
@@ -86,6 +86,8 @@ def proceed_dir(dir_path, dir_text, dir_path_forhtml):
                 AddInfo=AddInfo+", MQTT enabled";
             if ("_MDEVMAX" in bin_file) or (("WLEDMM_" in bin_file) and ("_max" in bin_file)):
                 AddInfo=AddInfo+", mods included: audio reactive, games, weather, custom effects, mpu6050 + other improvements";
+            if ("("WLEDSR_" in bin_file) and ("_max" in bin_file)):
+                AddInfo=AddInfo+", some additional mods included";
             if ("_DIGIN" in bin_file):
                 AddInfo=AddInfo+", digital line-in support";
             dict["ADDINFO"]=AddInfo;
@@ -125,7 +127,7 @@ for dir1 in dirs1:
         for dir2 in dirs2:
             dir2_path=os.path.join(dir1_path,dir2)
             if os.path.isdir(dir2_path):
-                dir2_text=dir1_text + " " + dir2.replace("_"," ")
+                dir2_text=dir1_text + ": " + dir2.replace("_"," ")
                 html_list = html_list + proceed_dir(dir2_path, dir2_text, "/"+bin_dir+"/"+dir1+"/"+dir2)
                 
 f_template=open("./scripts/index_template.html", "r")
